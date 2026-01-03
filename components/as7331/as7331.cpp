701e29dc3c0cb9094c9e32bde314ee12d29234b5
Cupra85 <daniel.curten@gmail.com>
2026-01-03 14:11:51 +0100

    Update as7331.cpp

--- components/as7331/as7331.cpp
+++ components/as7331/as7331.cpp
@@ -17,7 +17,15 @@ void AS7331Component::update() {
   }
 
   if (!wait_for_data_()) {
-    ESP_LOGW(TAG, "No data ready");
+    ESP_LOGD(TAG, "No new data, re-publishing last values");
+
+    if (uva_) uva_->publish_state(last_uva_);
+    if (uvb_) uvb_->publish_state(last_uvb_);
+    if (uvc_) uvc_->publish_state(last_uvc_);
+
+    if (uva_irr_) uva_irr_->publish_state(last_uva_ * uva_mult_);
+    if (uvb_irr_) uvb_irr_->publish_state(last_uvb_ * uvb_mult_);
+    if (uvc_irr_) uvc_irr_->publish_state(last_uvc_ * uvc_mult_);
     return;
   }
 
@@ -28,6 +36,10 @@ void AS7331Component::update() {
     ESP_LOGW(TAG, "Failed to read MRES registers");
     return;
   }
+
+  last_uva_ = m1;
+  last_uvb_ = m2;
+  last_uvc_ = m3;
 
   if (uva_) uva_->publish_state(m1);
   if (uvb_) uvb_->publish_state(m2);
@@ -40,21 +52,16 @@ void AS7331Component::update() {
 
 bool AS7331Component::write_cfg_() {
   uint8_t creg1 = (gain_ << 4) | (conv_time_ & 0x0F);
-  if (!this->write_byte(REG_CREG1, creg1)) return false;
-
-  uint8_t creg2 = divider_ & 0x07;
-  if (en_div_) creg2 |= 0x08;
-  if (!this->write_byte(REG_CREG2, creg2)) return false;
-
+  uint8_t creg2 = (divider_ & 0x07) | (en_div_ ? 0x08 : 0);
   uint8_t creg3 = (cclk_ & 0x03) | ((meas_mode_ & 0x03) << 6);
-  if (!this->write_byte(REG_CREG3, creg3)) return false;
 
-  return true;
+  return write_byte(REG_CREG1, creg1) &&
+         write_byte(REG_CREG2, creg2) &&
+         write_byte(REG_CREG3, creg3);
 }
 
 bool AS7331Component::start_measurement_() {
-  uint8_t osr = DOS_MEAS | OSR_SS;
-  return this->write_byte(REG_OSR, osr);
+  return write_byte(REG_OSR, DOS_MEAS | OSR_SS);
 }
 
 bool AS7331Component::wait_for_data_() {
@@ -69,7 +76,7 @@ bool AS7331Component::wait_for_data_() {
 
 bool AS7331Component::read_u16_(uint8_t reg, uint16_t &out) {
   uint8_t data[2];
-  if (!this->read_bytes(reg, data, 2)) return false;
+  if (!read_bytes(reg, data, 2)) return false;
   out = (uint16_t(data[1]) << 8) | data[0];
   return true;
 }
