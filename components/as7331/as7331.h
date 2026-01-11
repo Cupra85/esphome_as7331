ea07b6426f197f242a985f1750ecfb867430b975
Cupra85 <daniel.curten@gmail.com>
2026-01-11 17:42:43 +0100

    Update as7331.h

--- components/as7331/as7331.h
+++ components/as7331/as7331.h
@@ -9,27 +9,35 @@ namespace as7331 {
 
 class AS7331Component : public PollingComponent, public i2c::I2CDevice {
  public:
+  // Called from Python
   void set_gain(uint16_t gain) { gain_ = gain; }
-  void set_integration_time(uint16_t ms) { integration_time_ms_ = ms; }
+  void set_integration_time(uint16_t time_ms) { integration_time_ms_ = time_ms; }
 
+  // Sensors
   void set_uva_sensor(sensor::Sensor *s) { uva_ = s; }
   void set_uvb_sensor(sensor::Sensor *s) { uvb_ = s; }
   void set_uvc_sensor(sensor::Sensor *s) { uvc_ = s; }
   void set_uvi_sensor(sensor::Sensor *s) { uvi_ = s; }
 
-  void start_measurement();
-  void stop_measurement();
+  // Measurement control (used by switch)
+  void set_measurement_enabled(bool enable);
 
   void setup() override;
   void update() override;
 
  protected:
+  // Internal helpers
   bool configure_();
+  bool start_measurement_();
+  bool stop_measurement_();
   bool read_raw_(uint16_t &uva, uint16_t &uvb, uint16_t &uvc);
 
+  // Config
   uint16_t gain_{128};
   uint16_t integration_time_ms_{64};
+  bool measuring_{false};
 
+  // Sensors
   sensor::Sensor *uva_{nullptr};
   sensor::Sensor *uvb_{nullptr};
   sensor::Sensor *uvc_{nullptr};
