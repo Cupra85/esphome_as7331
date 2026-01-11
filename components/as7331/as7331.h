ad38a00f3be2e9ff3d4df12296db5115bb9b080d
Cupra85 <daniel.curten@gmail.com>
2026-01-11 22:09:42 +0100

    Refactor AS7331Component by removing redundant methods
    Removed unused sensor and configuration methods, and cleaned up member variables.

--- components/as7331/as7331.h
+++ components/as7331/as7331.h
@@ -9,70 +9,26 @@ namespace as7331 {
 
 class AS7331Component : public PollingComponent, public i2c::I2CDevice {
  public:
+  void set_gain(uint8_t gain) { gain_ = gain; }
+  void set_integration_time(uint8_t time) { integration_time_ = time; }
+
   void set_uva_sensor(sensor::Sensor *s) { uva_ = s; }
   void set_uvb_sensor(sensor::Sensor *s) { uvb_ = s; }
   void set_uvc_sensor(sensor::Sensor *s) { uvc_ = s; }
-
-  void set_uva_irr_sensor(sensor::Sensor *s) { uva_irr_ = s; }
-  void set_uvb_irr_sensor(sensor::Sensor *s) { uvb_irr_ = s; }
-  void set_uvc_irr_sensor(sensor::Sensor *s) { uvc_irr_ = s; }
-
-  void set_uva_multiplier(float v) { uva_mult_ = v; }
-  void set_uvb_multiplier(float v) { uvb_mult_ = v; }
-  void set_uvc_multiplier(float v) { uvc_mult_ = v; }
-
-  void set_gain(uint8_t g) { gain_ = g; }
-  void set_conversion_time(uint8_t t) { conv_time_ = t; }
-  void set_cclk(uint8_t c) { cclk_ = c; }
-  void set_measurement_mode(uint8_t m) { meas_mode_ = m; }
-  void set_enable_divider(bool e) { en_div_ = e; }
-  void set_divider(uint8_t d) { divider_ = d; }
 
   void setup() override;
   void update() override;
 
  protected:
-  static constexpr uint8_t REG_OSR   = 0x00;
-  static constexpr uint8_t REG_CREG1 = 0x06;
-  static constexpr uint8_t REG_CREG2 = 0x07;
-  static constexpr uint8_t REG_CREG3 = 0x08;
+  void configure_();
+  void start_measurement_();
 
-  static constexpr uint8_t REG_MRES1 = 0x02;
-  static constexpr uint8_t REG_MRES2 = 0x03;
-  static constexpr uint8_t REG_MRES3 = 0x04;
-
-  static constexpr uint8_t OSR_SS = 0x80;
-  static constexpr uint8_t DOS_MEAS = 0x03;
-
-  bool write_cfg_();
-  bool start_measurement_();
-  bool wait_for_data_();
-  bool read_u16_(uint8_t reg, uint16_t &out);
+  uint8_t gain_{4};
+  uint8_t integration_time_{6};
 
   sensor::Sensor *uva_{nullptr};
   sensor::Sensor *uvb_{nullptr};
   sensor::Sensor *uvc_{nullptr};
-
-  sensor::Sensor *uva_irr_{nullptr};
-  sensor::Sensor *uvb_irr_{nullptr};
-  sensor::Sensor *uvc_irr_{nullptr};
-
-  float uva_mult_{0};
-  float uvb_mult_{0};
-  float uvc_mult_{0};
-
-  uint8_t gain_{7};
-  uint8_t conv_time_{8};
-  uint8_t cclk_{0};
-  uint8_t meas_mode_{0};
-
-  bool en_div_{false};
-  uint8_t divider_{0};
-
-  // Cache gegen NaN
-  float last_uva_{0};
-  float last_uvb_{0};
-  float last_uvc_{0};
 };
 
 }  // namespace as7331
