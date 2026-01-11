ab292930cc05520a296a1e51dc8bd1eb741e0da6
Cupra85 <daniel.curten@gmail.com>
2026-01-11 18:38:03 +0100

    Clean up AS7331Component header file
    Removed commented-out code for clarity.

--- components/as7331/as7331.h
+++ components/as7331/as7331.h
@@ -9,24 +9,20 @@ namespace as7331 {
 
 class AS7331Component : public PollingComponent, public i2c::I2CDevice {
  public:
-  // YAML → C++
   void set_gain(uint16_t gain) { gain_ = gain; }
   void set_integration_time(uint16_t time_ms) { integration_time_ms_ = time_ms; }
 
-  // Sensor setter
   void set_uva_sensor(sensor::Sensor *s) { uva_ = s; }
   void set_uvb_sensor(sensor::Sensor *s) { uvb_ = s; }
   void set_uvc_sensor(sensor::Sensor *s) { uvc_ = s; }
   void set_uvi_sensor(sensor::Sensor *s) { uvi_ = s; }
 
-  // Switch API
   void set_measurement_enabled(bool enable);
 
   void setup() override;
   void update() override;
 
  protected:
-  // Internal helpers
   bool configure_();
   bool start_measurement_();
   bool stop_measurement_();
