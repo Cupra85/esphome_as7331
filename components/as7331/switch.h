b62ccba46c751acdc43af2adc2cf3c459ea77f9a
Cupra85 <daniel.curten@gmail.com>
2026-01-11 17:44:48 +0100

    Add AS7331Switch class for AS7331 component

---
+++ components/as7331/switch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "esphome/components/switch/switch.h"
+#include "as7331.h"
+
+namespace esphome {
+namespace as7331 {
+
+class AS7331Switch : public switch_::Switch {
+ public:
+  void set_parent(AS7331Component *parent) { parent_ = parent; }
+
+ protected:
+  void write_state(bool state) override {
+    if (parent_ != nullptr) {
+      parent_->set_measurement_enabled(state);
+    }
+    publish_state(state);
+  }
+
+  AS7331Component *parent_{nullptr};
+};
+
+}  // namespace as7331
+}  // namespace esphome
