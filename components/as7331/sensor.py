188c9d4053fcd4e0d93c6478ba333d1d1b3208c0
Cupra85 <daniel.curten@gmail.com>
2026-01-11 17:05:18 +0100

    Refactor AS7331Component configuration constants

--- components/as7331/sensor.py
+++ components/as7331/sensor.py
@@ -2,45 +2,44 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
 
+CONF_UVA = "uva"
+CONF_UVB = "uvb"
+CONF_UVC = "uvc"
+CONF_UV_INDEX = "uv_index"
+
 DEPENDENCIES = ["i2c"]
 
 as7331_ns = cg.esphome_ns.namespace("as7331")
+AS7331Component = as7331_ns.class_("AS7331Component", cg.PollingComponent, i2c.I2CDevice)
 
-AS7331Component = as7331_ns.class_(
-    "AS7331Component",
-    cg.Component,
-    i2c.I2CDevice,
-)
-
-PLATFORM_SCHEMA = (
-    sensor.PLATFORM_SCHEMA.extend(
+CONFIG_SCHEMA = (
+    cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(AS7331Component),
 
-            cv.Optional("uva"): sensor.sensor_schema(
+            cv.Optional(CONF_UVA): sensor.sensor_schema(
                 unit_of_measurement="W/m2",
-                icon="mdi:white-balance-sunny",
                 accuracy_decimals=3,
+                icon="mdi:white-balance-sunny",
             ),
-
-            cv.Optional("uvb"): sensor.sensor_schema(
+            cv.Optional(CONF_UVB): sensor.sensor_schema(
                 unit_of_measurement="W/m2",
-                icon="mdi:white-balance-sunny",
                 accuracy_decimals=3,
+                icon="mdi:white-balance-sunny",
             ),
-
-            cv.Optional("uvc"): sensor.sensor_schema(
+            cv.Optional(CONF_UVC): sensor.sensor_schema(
                 unit_of_measurement="W/m2",
-                icon="mdi:white-balance-sunny",
                 accuracy_decimals=3,
+                icon="mdi:white-balance-sunny",
             ),
-
-            cv.Optional("uv_index"): sensor.sensor_schema(
-                icon="mdi:weather-sunny-alert",
+            cv.Optional(CONF_UV_INDEX): sensor.sensor_schema(
+                unit_of_measurement="",
                 accuracy_decimals=2,
+                icon="mdi:weather-sunny-alert",
             ),
         }
     )
+    .extend(cv.polling_component_schema("10s"))
     .extend(i2c.i2c_device_schema(0x74))
 )
 
@@ -49,18 +48,18 @@ async def to_code(config):
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
-    if "uva" in config:
-        sens = await sensor.new_sensor(config["uva"])
-        cg.add(var.set_uva_sensor(sens))
+    if CONF_UVA in config:
+        s = await sensor.new_sensor(config[CONF_UVA])
+        cg.add(var.set_uva_sensor(s))
 
-    if "uvb" in config:
-        sens = await sensor.new_sensor(config["uvb"])
-        cg.add(var.set_uvb_sensor(sens))
+    if CONF_UVB in config:
+        s = await sensor.new_sensor(config[CONF_UVB])
+        cg.add(var.set_uvb_sensor(s))
 
-    if "uvc" in config:
-        sens = await sensor.new_sensor(config["uvc"])
-        cg.add(var.set_uvc_sensor(sens))
+    if CONF_UVC in config:
+        s = await sensor.new_sensor(config[CONF_UVC])
+        cg.add(var.set_uvc_sensor(s))
 
-    if "uv_index" in config:
-        sens = await sensor.new_sensor(config["uv_index"])
-        cg.add(var.set_uvi_sensor(sens))
+    if CONF_UV_INDEX in config:
+        s = await sensor.new_sensor(config[CONF_UV_INDEX])
+        cg.add(var.set_uvi_sensor(s))
