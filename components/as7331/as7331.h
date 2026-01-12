b095424faaa586cc6257ffcba8367c8998bbc62b
Cupra85 <daniel.curten@gmail.com>
2026-01-12 19:10:57 +0100

    Update as7331.h

--- components/as7331/as7331.h
+++ components/as7331/as7331.h
@@ -9,26 +9,13 @@ namespace as7331 {
 
 class AS7331Component : public PollingComponent, public i2c::I2CDevice {
  public:
-  void set_gain(uint8_t gain) { gain_ = gain; }
-  void set_integration_time(uint8_t time) { integration_time_ = time; }
-
   void set_uva_sensor(sensor::Sensor *s) { uva_ = s; }
-  void set_uvb_sensor(sensor::Sensor *s) { uvb_ = s; }
-  void set_uvc_sensor(sensor::Sensor *s) { uvc_ = s; }
 
   void setup() override;
   void update() override;
 
  protected:
-  void configure_();
-  void start_measurement_();
-
-  uint8_t gain_{4};
-  uint8_t integration_time_{6};
-
   sensor::Sensor *uva_{nullptr};
-  sensor::Sensor *uvb_{nullptr};
-  sensor::Sensor *uvc_{nullptr};
 };
 
 }  // namespace as7331
