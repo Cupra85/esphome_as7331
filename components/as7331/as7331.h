d27bd7369fa6c7d0cad9285d8b8c7700478c7ac0
Cupra85 <daniel.curten@gmail.com>
2026-01-13 18:16:08 +0100

    Update as7331.h

--- components/as7331/as7331.h
+++ components/as7331/as7331.h
@@ -9,31 +9,35 @@ namespace as7331 {
 
 class AS7331Component : public PollingComponent, public i2c::I2CDevice {
  public:
-  void set_uva_sensor(sensor::Sensor *s) { uva_ = s; }
-  void set_uvb_sensor(sensor::Sensor *s) { uvb_ = s; }
-  void set_uvc_sensor(sensor::Sensor *s) { uvc_ = s; }
+  void set_gain(uint8_t g) { gain_ = g; }
+  void set_int_time(uint8_t t) { int_time_ = t; }
+
+  void set_uva_raw_sensor(sensor::Sensor *s) { uva_raw_ = s; }
+  void set_uvb_raw_sensor(sensor::Sensor *s) { uvb_raw_ = s; }
+  void set_uvc_raw_sensor(sensor::Sensor *s) { uvc_raw_ = s; }
 
   void set_uva_wm2_sensor(sensor::Sensor *s) { uva_wm2_ = s; }
   void set_uvb_wm2_sensor(sensor::Sensor *s) { uvb_wm2_ = s; }
   void set_uvc_wm2_sensor(sensor::Sensor *s) { uvc_wm2_ = s; }
 
-  void set_gain(uint8_t g) { gain_ = g; }
-  void set_int_time(uint8_t t) { int_time_ = t; }
+  void set_uv_index_sensor(sensor::Sensor *s) { uv_index_ = s; }
 
   void setup() override;
   void update() override;
 
  protected:
-  uint8_t gain_{10};      // 0..11
-  uint8_t int_time_{6};  // 0..7
+  uint8_t gain_{3};
+  uint8_t int_time_{4};
 
-  sensor::Sensor *uva_{nullptr};
-  sensor::Sensor *uvb_{nullptr};
-  sensor::Sensor *uvc_{nullptr};
+  sensor::Sensor *uva_raw_{nullptr};
+  sensor::Sensor *uvb_raw_{nullptr};
+  sensor::Sensor *uvc_raw_{nullptr};
 
   sensor::Sensor *uva_wm2_{nullptr};
   sensor::Sensor *uvb_wm2_{nullptr};
   sensor::Sensor *uvc_wm2_{nullptr};
+
+  sensor::Sensor *uv_index_{nullptr};
 };
 
 }  // namespace as7331
