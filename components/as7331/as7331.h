065e19f038c95a97eb8acd695a3eef800c7527a5
Cupra85 <daniel.curten@gmail.com>
2026-01-18 16:56:13 +0100

    Update as7331.h

--- components/as7331/as7331.h
+++ components/as7331/as7331.h
@@ -14,7 +14,7 @@ class AS7331Component : public esphome::PollingComponent,
   void update() override;
   void dump_config() override;
 
-  // Setter (wichtig für ESPHome Codegen)
+  // Setter
   void set_uva_raw(sensor::Sensor *s) { uva_raw_ = s; }
   void set_uvb_raw(sensor::Sensor *s) { uvb_raw_ = s; }
   void set_uvc_raw(sensor::Sensor *s) { uvc_raw_ = s; }
@@ -25,11 +25,15 @@ class AS7331Component : public esphome::PollingComponent,
 
   void set_uv_index(sensor::Sensor *s) { uv_index_ = s; }
 
-  // Konfiguration (Defaultwerte)
-  uint8_t gain_ = 3;      // sinnvoller Mittelwert
-  uint8_t int_time_ = 4; // ca. 16 ms
-
  protected:
+  // bestehend
+  uint8_t gain_ = 3;
+  uint8_t int_time_ = 4;
+
+  // neu (Auto-Regler)
+  void auto_adjust_(uint16_t uva, uint16_t uvb, uint16_t uvc);
+  void write_config_();
+
   sensor::Sensor *uva_raw_{nullptr};
   sensor::Sensor *uvb_raw_{nullptr};
   sensor::Sensor *uvc_raw_{nullptr};
