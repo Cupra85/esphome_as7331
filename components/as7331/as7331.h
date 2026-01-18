c9b44df6a5fd9cb645db125894e3feb3274dbfac
Cupra85 <daniel.curten@gmail.com>
2026-01-18 16:08:00 +0100

    Update as7331.h

--- components/as7331/as7331.h
+++ components/as7331/as7331.h
@@ -7,20 +7,26 @@
 namespace esphome {
 namespace as7331 {
 
-class AS7331Component : public esphome::Component,
+class AS7331Component : public esphome::PollingComponent,
                         public esphome::i2c::I2CDevice {
  public:
+  // PollingComponent constructor (z. B. 10 s Update-Intervall)
+  AS7331Component() : PollingComponent(10000) {}
+
   void setup() override;
   void update() override;
 
+  // Raw sensors
   void set_uva_raw_sensor(sensor::Sensor *s) { uva_raw_ = s; }
   void set_uvb_raw_sensor(sensor::Sensor *s) { uvb_raw_ = s; }
   void set_uvc_raw_sensor(sensor::Sensor *s) { uvc_raw_ = s; }
 
+  // Irradiance sensors
   void set_uva_sensor(sensor::Sensor *s) { uva_wm2_ = s; }
   void set_uvb_sensor(sensor::Sensor *s) { uvb_wm2_ = s; }
   void set_uvc_sensor(sensor::Sensor *s) { uvc_wm2_ = s; }
 
+  // UV Index
   void set_uv_index_sensor(sensor::Sensor *s) { uv_index_ = s; }
 
  protected:
