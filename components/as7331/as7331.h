f491a8162202111d7835ccff1f6796ea8dab2e70
Cupra85 <daniel.curten@gmail.com>
2026-01-19 22:02:47 +0100

    Update as7331.h

--- components/as7331/as7331.h
+++ components/as7331/as7331.h
@@ -7,37 +7,34 @@
 namespace esphome {
 namespace as7331 {
 
-class AS7331Component : public PollingComponent,
-                        public i2c::I2CDevice {
+class AS7331Component : public esphome::PollingComponent,
+                        public esphome::i2c::I2CDevice {
  public:
   void setup() override;
   void update() override;
   void dump_config() override;
 
+  // Setter (wichtig für ESPHome Codegen)
+  // Setter
   void set_uva_raw(sensor::Sensor *s) { uva_raw_ = s; }
   void set_uvb_raw(sensor::Sensor *s) { uvb_raw_ = s; }
   void set_uvc_raw(sensor::Sensor *s) { uvc_raw_ = s; }
-
-  void set_uva(sensor::Sensor *s) { uva_ = s; }
-  void set_uvb(sensor::Sensor *s) { uvb_ = s; }
-  void set_uvc(sensor::Sensor *s) { uvc_ = s; }
+@@ -25,21 +25,25 @@
 
   void set_uv_index(sensor::Sensor *s) { uv_index_ = s; }
 
-  void set_uva_calibration(float v) { uva_cal_ = v; }
-  void set_uvb_calibration(float v) { uvb_cal_ = v; }
-  void set_uvc_calibration(float v) { uvc_cal_ = v; }
+  // Konfiguration (Defaultwerte)
+  uint8_t gain_ = 3;      // sinnvoller Mittelwert
+  uint8_t int_time_ = 4; // ca. 16 ms
 
  protected:
-  void write_config_();
+  // bestehend
+  uint8_t gain_ = 3;
+  uint8_t int_time_ = 4;
+
+  // neu (Auto-Regler)
   void auto_adjust_(uint16_t uva, uint16_t uvb, uint16_t uvc);
-
-  uint8_t gain_{3};
-  uint8_t int_time_{4};
-
-  float uva_cal_{1.0f};
-  float uvb_cal_{1.0f};
-  float uvc_cal_{1.0f};
+  void write_config_();
 
   sensor::Sensor *uva_raw_{nullptr};
   sensor::Sensor *uvb_raw_{nullptr};
